049a7ea97b34dbb11d8ab4301e6c8f07b5eac437
vala.avro <coren0327@gmail.com>
2026-02-13 18:35:51 +0100

    remove depreciated file

--- components/Sidebar.tsx
+++
@@ -1,9 +0,0 @@
-
-// This file is deprecated and can be safely removed or ignored as it is no longer imported by App.tsx.
-import React from 'react';
-
-const Sidebar: React.FC = () => {
-  return null;
-};
-
-export default Sidebar;
